4af4d125e2533fbf2d90886ebf9e178c38c0e5be
taobo <taobo@hashkey.cloud>
2026-06-25 18:22:16 +0800

    x

--- src/components/common/SecurityNotice/index.tsx
+++ src/components/common/SecurityNotice/index.tsx
@@ -2,6 +2,10 @@ import { useState, type ReactElement } from 'react'
 import { Alert, Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material'
 import WarningAmberIcon from '@mui/icons-material/WarningAmber'
 import useLocalStorage from '@/services/local-storage/useLocalStorage'
+import ExternalLink from '@/components/common/ExternalLink'
+
+// HashKey Safe 帮助文档
+const HELP_DOC_URL = 'https://docs.hashkeychain.net/docs/Build-on-HashKey-Chain/Tools/Safe'
 
 // 用户确认过安全须知后写入, 避免每次进入都弹窗
 const ACK_KEY = 'securityNoticeAcknowledged_v1'
@@ -26,15 +30,16 @@ const SecurityNotice = (): ReactElement => {
       </Alert>
 
       {/* 首次进入展示的安全须知弹窗 */}
-      <Dialog open={open} onClose={handleClose} maxWidth="xs">
-        <DialogTitle>Security notice</DialogTitle>
+      <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+        <DialogTitle sx={{ fontSize: '1.25rem', pt: 3 }}>Security notice</DialogTitle>
         <DialogContent>
-          <Typography variant="body2" mb={2}>
+          <Typography mb={2}>
             Due to recent security incidents it is important to ALWAYS verify transactions that you are approving on
             your signer wallet. If you can&apos;t verify it, don&apos;t sign it.
           </Typography>
-          <Typography variant="body2">
-            More information on how to verify a transaction can be found in the <b>help center</b>.
+          <Typography>
+            More information on how to verify a transaction can be found in the{' '}
+            <ExternalLink href={HELP_DOC_URL}>corresponding help center article</ExternalLink>.
           </Typography>
         </DialogContent>
         <DialogActions>
